1304fa7ea07486951d97dcce8ee120e6d2e19e9f
collinorendorff <co2fc@virginia.edu>
2022-09-30 14:49:29 -0400

    Changed the navbar to include fields to enter username and password; added login/logout views and urls

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -33,12 +33,26 @@ def contact(request):
         return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        pw = request.POST['psw']
+        user = authenticate(username=username, password=pw)
+
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['notice'] = "Invalid username or password"
+            return render(request, 'django/login.html', context)
+    else:
+        return render(request, 'djangoapp/login.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):

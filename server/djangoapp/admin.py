c076a94323850c1f8fa1037349d766db6b95fa12
collinorendorff <co2fc@virginia.edu>
2022-10-01 16:47:48 -0400

    Added first 2 models to models.py and created corresponding models in admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,9 +5,18 @@ from django.contrib import admin
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['name', 'cardealer', 'cartype', 'caryear']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from django.utils.timezone import now
 
 
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+#  Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='')
+    description = models.CharField(max_length=500)
+    def __str__(self):
+        return "Name: " + self.name + "," + "Description: " + self.description
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
@@ -19,9 +21,26 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
-
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+class CarModel(models.Model):
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    cardealer = models.IntegerField()
+    name = models.CharField(null=False, max_length=50, default='')
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    OTHER = 'other'
+    CAR_CHOICES = [
+        (SEDAN, "Sedan"),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (OTHER, 'Other')
+        ]
+    cartype = models.CharField(
+        null=False,
+        max_length=20,
+        choices=CAR_CHOICES,
+        default=SEDAN
+    )
+    caryear = models.DateField()
+    def __str__(self):
+        return "Name: " + self.name + "," + "Type: " + self.cartype
